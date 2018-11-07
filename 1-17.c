645a38ed611db792ad496c04e5115776455ff371
guowangbixia <32806282+guowangbixia@users.noreply.github.com>
2018-11-08 01:19:18 +0800

    Add files via upload

---
+++ 1-17.c
@@ -0,0 +1,24 @@
+//input the line that longer than 80 characters
+#include<stdio.h>
+#define MAXSIZE 1000
+#define THRESHOLD 80
+int main()
+{
+	char c;
+	int num=0;
+	printf("please input the text:\n");
+	do {
+		char a[MAXSIZE]="";
+		while( (c=getchar())!=EOF &&c!='\n' ){
+			a[num]=c;
+			num++;
+		}
+	    if(num>=THRESHOLD){
+			printf("\n%s\n",a);
+			num=0;
+			putchar('\n');
+		}
+	} 	while(c!=EOF);
+	printf("exit\n");
+	return 0;
+}
